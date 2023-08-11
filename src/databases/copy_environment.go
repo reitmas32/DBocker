c26ce6fd63c1438c7e14e4623fab9fa2f9f73791
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-11 01:55:38 -0600

    feat: create CopyDB_Project func

---
+++ src/databases/copy_environment.go
@@ -0,0 +1,29 @@
+package databases
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/UniHacksOrg/DBocker/src/models"
+	"github.com/UniHacksOrg/DBocker/src/tools"
+)
+
+func CopyDB_Project(base string, config models.DB_Config) string {
+	// Obtener la ruta del directorio actual
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error al obtener la ruta del directorio actual:", err)
+		return "error"
+	}
+	sourceFolder := filepath.Join(currentDir, "..", "assets", base)
+	destinationFolder := filepath.Join(currentDir, "sandbox", config.Name)
+
+	err = tools.CopyFolder(sourceFolder, destinationFolder)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Carpeta copiada exitosamente")
+	}
+	return destinationFolder
+}
